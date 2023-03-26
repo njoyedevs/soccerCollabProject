1b094aa24b83d892903d8d9d1683d80debd057a9
Nicolas Joye <nicolasajoye@gmail.com>
2023-03-26 16:16:34 -0700

    Updated hamburger menu and env

--- apps/client/src/App.js
+++ apps/client/src/App.js
@@ -2,46 +2,74 @@ import './App.css';
 import Standings from './components/StandingsComponent';
 import Games from './components/GamesComponent';
 import Game from './components/GameComponent';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faFutbol, faStar, faNewspaper } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faFutbol, faStar, faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
+import React from 'react';
 
-const APIKEY = "";
-
+const APIKEY = '';
 
 function App() {
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <Button
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+      {children}
+    </Button>
+  ));
+
   return (
     <div className="App">
       <header className="top-menu">
-        <nav className='navbar'>
-          <div className='icons'>
-            <div className="nav-item dropdown">
-              <a className="nav-link dropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <FontAwesomeIcon icon={faBars} />
-              </a>
-              <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">About the Developers</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
-              </ul>
+        <nav className="navbar">
+          <div className="icons">
+            <div className="dropDown">
+              <Dropdown>
+                <Dropdown.Toggle
+                  style={{ backgroundColor: 'black', border: 'none' }}
+                  variant="secondary"
+                  id="dropdown-basic"
+                >
+                  <FontAwesomeIcon icon={faBars} />
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </div>
             <h5>FootyStats</h5>
           </div>
-          <div className='icons'>
-            <p><FontAwesomeIcon icon={faFutbol} /></p>
+          <div className="icons">
+            <p>
+              <FontAwesomeIcon icon={faFutbol} />
+            </p>
             <h5>Scores</h5>
           </div>
-          <div className='icons'>
-            <p><FontAwesomeIcon icon={faStar} /></p>
+          <div className="icons">
+            <p>
+              <FontAwesomeIcon icon={faStar} />
+            </p>
             <h5>Favorites</h5>
           </div>
-          <div className='icons'>
-            <p><FontAwesomeIcon icon={faNewspaper} /></p>
+          <div className="icons">
+            <p>
+              <FontAwesomeIcon icon={faNewspaper} />
+            </p>
             <h5>News</h5>
           </div>
-          <button type="button" class="btn btn-outline-light" id="login-button">Login/Signup</button>
+          <button type="button" class="btn btn-outline-light" id="login-button" as={CustomToggle}>
+            Login/Signup
+          </button>
         </nav>
       </header>
-
 
       <div className="middleRow">Middle R - Insert Here</div>
       <div className="bottomCol">
