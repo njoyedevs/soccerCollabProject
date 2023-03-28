295b336cf7e38270dc93b71396d3c3994f0f9018
Nicolas Joye <nicolasajoye@gmail.com>
2023-03-27 18:06:11 -0700

    Added Backend Files

---
+++ apps/api/src/routes/product.routes.js
@@ -0,0 +1,15 @@
+const ProductController = require('../controllers/product.controller');
+
+module.exports = (app) => {
+  // app.get('/api', ProductController.index);
+  // app.get('/api/products', ProductController.getAllProducts);
+  // app.get('/api/product/:id', ProductController.getProduct);
+  // app.put('/api/product/:id', ProductController.updateProduct);
+  // app.post('/api/new/product', ProductController.createProduct);
+  // app.delete('/api/product/:id', ProductController.deleteProduct);
+  app.get('/api', ProductController.getAllProducts); // Read
+  app.get('/api/:id', ProductController.getProduct); // Show 1
+  app.put('/api/edit/:id', ProductController.updateProduct); // Update
+  app.post('/api/new/', ProductController.createProduct); // Create
+  app.delete('/api/delete/:id', ProductController.deleteProduct); // Delete
+};
--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -1,1 +1,18 @@
-console.log('server started');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+require('./config/mongoose.config');
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json(), express.urlencoded({ extended: true })); // This is new
+require('./routes/product.routes')(app);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.status(500).json({ ...error, message: error.message });
+});
+app.listen(port, () => console.log(`Listening on port: ${port}`));
