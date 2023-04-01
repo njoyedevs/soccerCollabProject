0ac5506bc4549f27e14ddb0d6dc32e5b018427e7
njoyedevs <118856885+njoyedevs@users.noreply.github.com>
2023-03-31 18:20:27 -0700

    Updated fil

--- apps/api/src/config/mongoose.config.js
+++ apps/api/src/config/mongoose.config.js
@@ -2,12 +2,10 @@ const { urlencoded } = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const replaceThisUrl =
-  'mongodb+srv://nicolasajoye:Njs7SktJmT8Ud3Aa@jokesapi.sjpkgmx.mongodb.net/SoccerTrackerDB?retryWrites=true&w=majority';
 const URL = process.env.MONGODB_URL;
 
 mongoose
-  .connect(replaceThisUrl, {
+  .connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
